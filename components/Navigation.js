a5fddfdb5e1ccb7ebba6b07c40365536db8bcc9c
Ralitsa Popova <ralicaterzieva90@gmail.com>
2023-11-04 18:26:30 +0200

    Add Daisy UI

--- components/Navigation.js
+++ components/Navigation.js
@@ -2,12 +2,13 @@ import Link from 'next/link';
 
 function NavBar() {
   return (
-    <nav className="bg-blue-500 p-4">
+    <nav className="bg-blue-500 p-4 flex justify-between items-center">
       <div className="container mx-auto">
-        <Link href="/" className="text-3xl text-white font-bold">
-         DevDive
-        </Link>
+        <Link href="/" className="text-3xl text-white font-bold">DevDive</Link>
       </div>
+      <Link href="/blogs">
+        <button className="btn btn-info w-64 rounded-full text-white">Blogs</button>
+      </Link>
     </nav>
   );
 }
